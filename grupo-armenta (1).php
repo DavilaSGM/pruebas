cf56776e5bbc2e3cf3ffac9294dfdae714de5f4b
DavilaSGM <ddavila.sgm@gmail.com>
2025-08-19 13:20:52 -0600

    Add files via upload

---
+++ grupo-armenta (1).php
@@ -0,0 +1,229 @@
+<!DOCTYPE html>
+<html>
+     <head>
+          <meta charset="utf-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1">
+          <title>Grupo Armenta</title>
+          <meta name="description" content="Somos una agencia con más de 16 años de experiencia implementando estrategias que garantizan la omnicanalidad, enfocadas en cautivar a usuarios para crear experiencias, fidelización mediante herramientas óptimas e incremento de ventas.">          
+          <meta property="og:image" content="http://grupoarmenta.com/og-grupo-armenta.jpg">
+          <meta property="og:image:secure_url" content="https://grupoarmenta.com/og-grupo-armenta.jpg">
+          <?php include 'assets/includes/head.php';?>
+
+          <link rel="stylesheet" type="text/css" href="assets/css/grupo-armenta.css">
+     </head>
+
+     <body class="nav-grupo-armenta">
+
+     <?php include 'assets/includes/menu.php';?>
+
+          <section class="section">
+               <div class="container fixed">
+                    <div class="row justify-content-center">
+                         <div class="col-12 col-sm-11 col-md-8 col-lg-7 col-xl-6 position-realative">
+                              <div class="align-middle d-flex">
+                                   <div class="hashtag-hacemos-grupo">
+                                        #hacemosquesuceda
+                                        <!-- <img class="width-20px" src="assets/img/imagotipo-armenta.svg" alt="Grupo Armenta"> -->
+                                   </div>
+                                   <div>
+                                        <h1 class="title-ga-float-grupo text-uppercase font-weight-600 margin-20px-bottom service-title line-height-80"> <!-- title-large -->
+                                             <span class="text-blue">Grupo</span><br class=""><span class="text-purple">Armenta</span>
+                                        </h1>
+                                   </div>
+                              </div>
+                              <div>
+                                   <p class="font-weight-300 text-main d-none d-sm-block">Somos una agencia con más de 16 años de experiencia implementando estrategias que garantizan la omnicanalidad, enfocadas en cautivar a usuarios para crear experiencias, fidelización mediante herramientas óptimas e incremento de ventas.</p>
+                                   <p class="font-weight-300 text-main">Contamos con 6 áreas que se complementan entre sí para trazar el camino al éxito, estas son: Trade marketing, Distribución y logística, Ecommerce, Marketing digital, Laboratorio Creativo y Eventos.</p>
+                              </div>
+                         </div>
+                         <div class="col-9 col-sm-6 col-md-4 col-lg-4 col-xl-3 text-center">
+                              <img class="width-100 max-width-500px lg-width-90 md-width-80 xs-width-70" src="assets/img/imagotipo-armenta.svg" alt="Grupo Armenta">
+                              <!-- <a class="btn btn-alt margin-20px-top" href="">Más sobre Grupo Armenta</a> -->
+                         </div>
+                    </div>
+               </div>
+          </section>
+
+          <section class="section">
+               <div class="container fixed">
+                    <div class="row justify-content-center">
+                         <div class="col-12 col-sm-10 order-md-last col-md-6 col-lg-6 col-xl-5 d-flex align-items-center">
+                              <div class="brands-head-block-text sm-no-padding-bottom">
+                                   <div class="row">
+                                        <div class="col-12">
+                                             <div class="d-flex justify-content-between align-items-end">
+                                                  <div>
+                                                       <img class="margin-20px-bottom width-100 max-width-250px" src="assets/img/brands/logo-loquiero-blanco.svg" alt="LoQuiero">
+                                                  </div>
+                                                  <div class="d-flex icons-rrss-white-brands-main">
+                                                       <a class="rrss-brands-fb margin-1px-lr" href="https://www.facebook.com/loquiero.marketplace" target="_blank">
+                                                            <img class="" src="assets/img/rrss/fb-white.svg" alt="Facebook">
+                                                       </a>
+                                                       <a class="rrss-brands-ig margin-1px-lr" href="https://www.instagram.com/loquiero.marketplace/" target="_blank">
+                                                            <img class="" src="assets/img/rrss/ig-white.svg" alt="Instagram">
+                                                       </a>
+                                                       <!-- <a class="rrss-yt margin-1px-lr" href="#">
+                                                            <img class="" src="assets/img/rrss/yt-white.svg" alt="YouTube">
+                                                       </a>
+                                                       <a class="rrss-in margin-1px-lr" href="#">
+                                                            <img class="" src="assets/img/rrss/in-white.svg" alt="LinkedIn">
+                                                       </a> -->
+                                                  </div>
+                                             </div>
+                                        </div>
+                                        <div class="text-left">
+                                             <p class="text-alt">Si estás interesado en vender tus productos vía online, nos encargamos de potencializar a tu marca en el universo del ecommerce con nuestra propia plataforma. ¡Descubre cómo podemos llevarla al siguiente nivel!</p>
+                                        </div>
+                                        <div>
+                                             <a class="btn btn-main margin-10px-top md-margin-40px-bottom" href="loquieromx.php">Explorar</a>
+                                        </div>
+                                   </div>
+                              </div>
+                         </div>
+                         <div class="col-9 col-sm-8 order-md-first col-md-6 col-lg-6 col-xl-6 d-flex align-items-center">
+                              <img class="" src="assets/img/brands/img-loquiero.png" alt="LoQuiero.mx">
+                         </div>
+                    </div>
+               </div>
+          </section>
+
+          <section class="section">
+               <div class="container fixed">
+                    <div class="row justify-content-center">
+                         <div class="col-12 col-sm-10 order-md-first col-md-6 col-lg-6 col-xl-5 d-flex align-items-center">
+                              <div class="brands-head-block-text sm-no-padding-bottom">
+                                   <div class="row">
+                                        <div class="col-12">
+                                             <div class="d-flex justify-content-between align-items-end">
+                                                  <div>
+                                                       <img class="margin-20px-bottom width-100 max-width-200px" src="assets/img/brands/logo-marketing-blanco.svg" alt="Marketing 361">
+                                                  </div>
+                                             </div>
+                                        </div>
+                                        <div class="text-left">
+                                             <p class="text-alt">Si estás listo para llevar tu marca al siguiente nivel, te invitamos a unirte a nosotros en el emocionante mundo del marketing 361. Juntos, exploraremos nuevas posibilidades, crearemos conexiones significativas y alcanzaremos alturas inimaginables en el reino digital. ¡Bienvenidos a Marketing 361, donde la innovación y el éxito convergen en cada grado!</p>
+                                        </div>
+                                        <div>
+                                             <a class="btn btn-main margin-10px-top md-margin-40px-bottom" href="marketing361.php">Explorar</a>
+                                        </div>
+                                   </div>
+                              </div>
+                         </div>
+                         <div class="col-9 col-sm-8 order-md-last col-md-6 col-lg-6 col-xl-6 d-flex align-items-center">
+                              <img class="" src="assets/img/brands/img-marketing.png" alt="Marketing 361">
+                         </div>
+                    </div>
+               </div>
+          </section>
+
+          <section class="section">
+               <div class="container fixed">
+                    <div class="row justify-content-center">
+                         <div class="col-12 col-sm-10 order-md-last col-md-6 col-lg-6 col-xl-5 d-flex align-items-center">
+                              <div class="brands-head-block-text sm-no-padding-bottom">
+                                   <div class="row">
+                                        <div class="col-12">
+                                             <div class="d-flex justify-content-between align-items-end">
+                                                  <div>
+                                                       <img class="margin-20px-bottom width-100 max-width-200px" src="assets/img/brands/logo-sgm-blanco.svg" alt="SGM">
+                                                  </div>
+                                                  <div class="d-flex icons-rrss-white-brands-main">
+                                                       <a class="rrss-brands-fb margin-1px-lr" href="https://www.facebook.com/sgm.talento" target="_blank">
+                                                            <img class="" src="assets/img/rrss/fb-white.svg" alt="Facebook">
+                                                       </a>
+                                                       <a class="rrss-brands-ig margin-1px-lr" href="https://www.instagram.com/sgm.talento/" target="_blank">
+                                                            <img class="" src="assets/img/rrss/ig-white.svg" alt="Instagram">
+                                                       </a>
+                                                       <!-- <a class="rrss-brands-yt margin-1px-lr" href="#">
+                                                            <img class="" src="assets/img/rrss/yt-white.svg" alt="YouTube">
+                                                       </a>
+                                                       <a class="rrss-brands-in margin-1px-lr" href="#">
+                                                            <img class="" src="assets/img/rrss/in-white.svg" alt="LinkedIn">
+                                                       </a> -->
+                                                  </div>
+                                             </div>
+                                        </div>
+                                        <div class="text-left">
+                                             <p class="text-alt">Brindamos un servicio integral de ejecución en punto de venta con personal experto que ofrece las herramientas necesarias para tu marca, apoyando en una mejor exibición de los productos seleccionados. Profundizamos nuestro servicio con el alcance de objetivos de venta, siempre enriqueciendo las relaciones entre los puntos de promoción y la marca.</p>
+                                        </div>
+                                        <div>
+                                             <a class="btn btn-main margin-10px-top md-margin-40px-bottom" href="sgm.php">Explorar</a>
+                                        </div>
+                                   </div>
+                              </div>
+                         </div>
+                         <div class="col-9 col-sm-8 order-md-first col-md-6 col-lg-6 col-xl-6 d-flex align-items-center">
+                              <img class="" src="assets/img/brands/img-sgm.png" alt="SGM">
+                         </div>
+                    </div>
+               </div>
+          </section>
+
+          <section class="section">
+               <div class="container fixed">
+                    <div class="row justify-content-center">
+                         <div class="col-12 col-sm-10 order-md-first col-md-6 col-lg-6 col-xl-5 d-flex align-items-center">
+                              <div class="brands-head-block-text sm-no-padding-bottom">
+                                   <div class="row">
+                                        <div class="col-12">
+                                             <div class="d-flex justify-content-between align-items-end">
+                                                  <div>
+                                                       <img class="margin-20px-bottom width-100 max-width-200px" src="assets/img/brands/logo-grid-blanco.svg" alt="GRID RA">
+                                                  </div>
+                                             </div>
+                                        </div>
+                                        <div class="text-left">
+                                             <p class="text-alt">Nos esforzamos por mantener los más altos estándares para satisfacer tus expectativas.</p>
+                                        </div>
+                                        <div>
+                                             <a class="btn btn-main margin-10px-top md-margin-40px-bottom" href="gridra.php">Explorar</a>
+                                        </div>
+                                   </div>
+                              </div>
+                         </div>
+                         <div class="col-9 col-sm-8 order-md-last col-md-6 col-lg-6 col-xl-6 d-flex align-items-center">
+                              <img class="" src="assets/img/brands/img-grid.png" alt="Grid">
+                         </div>
+                    </div>
+               </div>
+          </section>
+
+          <section class="container-fluid section">
+               <div class="container fixed padding-40px-bottom sm-padding-10px-bottom">
+                    <div class="row justify-content-center">
+                         <div class="col-md-12 col-lg-10 col-xl-9">
+                              <div class="d-flex flex-column flex-md-row">
+                                   <div class="w-100 cta-text">
+                                        <span class="text-small text-blue">Contáctanos</span>
+                                        <h2 class="text-uppercase font-weight-600 margin-10px-bottom line-height-50">
+                                             <span class="text-gradient">Te ayudamos a llegar</span>
+                                             <!-- <br class="d-none d-sm-block "/> -->
+                                             <br/>
+                                             <span class="banner-square padding-10px-lr">a más clientes potenciales</span></h2>
+                                        <p class="text-medium">Cubrimos las necesidades de tu marca, creando la estrategia de marketing perfecta para tu producto o servicio.</p>
+                                   </div>
+                                   <div class="d-flex align-items-center">
+                                        <a class="btn btn-alt" href="contacto.php">Contacto</a>
+                                   </div>
+                              </div>
+                         </div>
+                    </div>
+               </div>
+               <div class="container-fluid bg-gray fixed-bottom">
+                    <?php include 'assets/includes/rrss.php';?>
+                    <!-- footer -->
+                    <div class="row justify-content-center">
+                         <?php include 'assets/includes/footer.php';?>
+                    </div>
+               </div>
+          </section>
+
+          
+
+               <!-- scripts -->
+               <?php include 'assets/includes/scripts.php';?>
+               
+               <!-- <script src="assets/js/index.js"></script> -->
+          <script src="assets/js/global.js"></script>
+     </body>
+</html>
